65161f0320cbb79b56cdf05d730fdeda8ee1239e
Zakariyya Mughal <zaki.mughal@gmail.com>
2019-11-08 18:55:17 -0600

    Initial skeleton

---
+++ lib/Renard/API/CEF.pm
@@ -0,0 +1,12 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::API::CEF;
+# ABSTRACT: API for Chromium Embedded Framework
+
+use Mu;
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-API-CEF/>
+
+=cut
